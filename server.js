79ab3647bd193649524b339c2ac21a93ac818487
Hugo Tamayo <doctamayot@hotmail.com>
2021-02-04 22:20:00 -0500

    desplegando v12

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 const { readdirSync } = require('fs');
 require('dotenv').config();
 
@@ -27,7 +26,17 @@ mongoose
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: '2mb' }));
-app.use(cors());
+//Configure Header HTTP
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // routes middleware
 // routes middleware
