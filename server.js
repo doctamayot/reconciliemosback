0e775f3f2165d4bfb53b1904dba92e45da20364e
Hugo Tamayo <doctamayot@hotmail.com>
2024-08-26 21:27:51 -0500

    headers

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const app = express();
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
   })
   .then(() => console.log("DB CONNECTED"))
